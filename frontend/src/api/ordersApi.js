4cf2415160a8dbd69e6c70af59a90e320cfd7c02
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2026-03-04 22:05:09 -0800

    using vite_api_url for api base urk

--- frontend/src/api/ordersApi.js
+++ frontend/src/api/ordersApi.js
@@ -1,4 +1,4 @@
-const API_BASE = "/api";
+const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
 
 function toQuery(params){
     const sp = new URLSearchParams(); //helps build query strings safely
